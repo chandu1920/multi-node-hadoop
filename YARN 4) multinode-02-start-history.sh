8a4d92dccd0245e74f3bef2442dc63839d170756
chandu1920 <36391231+chandu1920@users.noreply.github.com>
2018-08-20 19:26:00 +0530

    Create YARN 4) multinode-02-start-history.sh

---
+++ YARN 4) multinode-02-start-history.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.171-8.b10.el7_5.x86_64
+export HADOOP_CONF_DIR=/etc/hadoop/conf
+export HADOOP_LOG_DIR=/var/log/hadoop
+/opt/hadoop-2.9.1/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start ${1}
