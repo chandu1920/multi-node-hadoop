aa78ff073e11618fa10fa10f8a87abdbc5e8d1b3
chandu1920 <36391231+chandu1920@users.noreply.github.com>
2018-08-20 19:03:45 +0530

    Create 4 multinode-sshpass.sh

---
+++ 4 multinode-sshpass.sh
@@ -0,0 +1,7 @@
+sshpass -f <(printf '%s\n' itversity) ssh-copy-id -o StrictHostKeyChecking=no hadoop@abc006
+sshpass -f <(printf '%s\n' itversity) ssh-copy-id -o StrictHostKeyChecking=no hadoop@abc005
+sshpass -f <(printf '%s\n' itversity) ssh-copy-id -o StrictHostKeyChecking=no hadoop@abc004
+sshpass -f <(printf '%s\n' itversity) ssh-copy-id -o StrictHostKeyChecking=no hadoop@abc003
+sshpass -f <(printf '%s\n' itversity) ssh-copy-id -o StrictHostKeyChecking=no hadoop@abc002
+sshpass -f <(printf '%s\n' itversity) ssh-copy-id -o StrictHostKeyChecking=no hadoop@abc001
+sshpass -f <(printf '%s\n' itversity) ssh-copy-id -o StrictHostKeyChecking=no hadoop@abc000
