9c146418e883e62f3b868c9dac08d5fe6bf93191
chandu1920 <36391231+chandu1920@users.noreply.github.com>
2018-08-20 19:06:27 +0530

    Create multinode-02-firewall_reload_on_all_nodes.sh

---
+++ multinode-02-firewall_reload_on_all_nodes.sh
@@ -0,0 +1,7 @@
+ssh hadoop@abc000 'bash -s' < firewall_reload.sh 
+ssh hadoop@abc001 'bash -s' < firewall_reload.sh 
+ssh hadoop@abc002 'bash -s' < firewall_reload.sh 
+ssh hadoop@abc003 'bash -s' < firewall_reload.sh 
+ssh hadoop@abc004 'bash -s' < firewall_reload.sh 
+ssh hadoop@abc005 'bash -s' < firewall_reload.sh 
+ssh hadoop@abc006 'bash -s' < firewall_reload.sh
